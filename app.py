5c50e27e8582666bf1a6e5e2e8ca01ea496a3d0c
amir.k <amir.k@taboola.com>
2017-11-09 21:17:51 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, render_template, request, redirect, url_for
+from slackclient import SlackClient
+from logic import create_dataframe, process_dataframe, get_restaurants_by_average_time, get_n_most_popular_restaurants, \
+  get_n_earliest_restaurants, get_n_latest_restaurants, get_average_time_for_restaurant
+
+import os
+import json
+
+app = Flask(__name__)
+
+VERIFICATION_TOKEN = os.environ.get('VERIFICATION_TOKEN')
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+
+def get_messages(slack_client, count=1000):
+  history = slack_client.api_call(
+    'channels.history',
+    channel='C37ELNXTK',
+    count=count
+  )
+  return history['messages']
+
+slack_client = SlackClient(ACCESS_TOKEN)
+
+messages = get_messages(slack_client=slack_client)
+dataframe = create_dataframe(messages=messages)
+grouped_dataframe = process_dataframe(dataframe=dataframe)
+
+@app.route('/slack_event', methods=['POST'])
+def slack_event():
+  req = json.loads(request.data)
+  if req.get('token') == VERIFICATION_TOKEN:
+    if 'challenge' in req:
+      return req.get('challenge')
+    event = req.get('event')
+    if not event.get('bot_id') and 'text' in event:
+      attachments = [
+        {
+          "text": "Choose one of the following options",
+          "fallback": "You are unable to choose an option",
+          "callback_id": "foodbot_selection",
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "actions": [
+            {
+              "name": "foodbot",
+              "text": "Most Popular",
+              "type": "button",
+              "value": "popular"
+            },
+            {
+              "name": "foodbot",
+              "text": "Arrives Earliest",
+              "type": "button",
+              "value": "earliest"
+            },
+            {
+              "name": "foodbot",
+              "text": "Arrives Latest",
+              "type": "button",
+              "value": "latest"
+            },
+            {
+              "name": "foodbot",
+              "text": "Before 12:30",
+              "type": "button",
+              "value": "first_half"
+            },
+            {
+              "name": "foodbot",
+              "text": "After 12:30",
+              "type": "button",
+              "value": "second_half"
+            }
+          ]
+        }
+      ]
+      slack_client.api_call(
+        'chat.postMessage',
+        channel=event.get('channel'),
+        text='',
+        attachments=json.dumps(attachments)
+      )
+    return ''
+
+@app.route('/slack_action', methods=['POST'])
+def slack_action():
+  form_json = json.loads(request.form['payload'])
+  if form_json.get('token') == VERIFICATION_TOKEN:
+    selection = form_json.get('actions')[0].get('value')
+    slack_client.api_call(
+      'chat.update',
+      channel=form_json.get('channel').get('id'),
+      ts=form_json.get('message_ts'),
+      text=get_results_from_selection(selection=selection),
+      attachments=[]
+    )
+  return ''
+
+@app.route('/debug', methods=['GET'])
+def debug():
+  return str(grouped_dataframe)
+
+def get_results_from_selection(selection):
+  if selection == 'popular':
+    return get_n_most_popular_restaurants(dataframe=grouped_dataframe)
+  elif selection == 'earliest':
+    return get_n_earliest_restaurants(dataframe=grouped_dataframe)
+  elif selection == 'latest':
+    return get_n_latest_restaurants(dataframe=grouped_dataframe)
+  elif selection == 'first_half':
+    return get_restaurants_by_average_time(dataframe=grouped_dataframe, start_hour='11:00', end_hour='12:30')
+  elif selection == 'second_half':
+    return get_restaurants_by_average_time(dataframe=grouped_dataframe, start_hour='12:30', end_hour='14:00')
+  return ''
+
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
---
+++ logic.py
@@ -0,0 +1,99 @@
+from datetime import datetime as dt
+
+import pandas as pd
+import time as tm
+
+TIME_ZONE = 'Asia/Baghdad'
+
+def accept_message(message):
+    to_filter = ['has joined the channel', 'uploaded a file', '?', '#', '@', '(', ')']
+    def is_english(message):
+        try:
+            message.encode(encoding='utf-8').decode('ascii')
+        except UnicodeDecodeError:
+            return False
+        else:
+            return True
+    return not any(x in message[0] for x in to_filter) and not is_english(message[0])
+
+def clean_message(message):
+    return message.strip().replace('!', '')
+    
+def preprocess_messages(messages, max_length=20):
+    messages = [(clean_message(message['text']),message['ts']) 
+                for message in messages if message['type'] == 'message']
+    messages = [message for message in messages if accept_message(message)]
+    split_messages_new_line = []
+    for message in messages:
+        for _message in message[0].split('\n'):
+            split_messages_new_line.append((_message, message[1]))
+    split_messages_comma = []    
+    for message in split_messages_new_line:
+        for _message in message[0].split(','):
+            split_messages_comma.append((_message, message[1]))
+    messages = [message for message in split_messages_comma if len(message[0]) < max_length]
+    return messages
+
+def get_restaurants_and_timestamps(messages):
+    return preprocess_messages(messages)
+
+def normalize_timestamps(restaurants_and_timestamps):
+    updated_restaurants_and_timestamps = []
+    for restaurant_and_timestamp in restaurants_and_timestamps:
+        old_epoch = int(str(restaurant_and_timestamp[1].split('.')[0]))
+        new_epoch = tm.mktime(dt.fromtimestamp(old_epoch).replace(year=1983, month=12, day=9).timetuple())
+        updated_restaurants_and_timestamps.append((restaurant_and_timestamp[0], new_epoch))
+    return updated_restaurants_and_timestamps
+
+def create_dataframe(messages):
+    restaurants_and_timestamps = normalize_timestamps(get_restaurants_and_timestamps(messages=messages))
+    return pd.DataFrame(restaurants_and_timestamps, columns=['restaurant', 'timestamp'])
+    
+def process_dataframe(dataframe, min_threshold=4):    
+    dataframe = dataframe.groupby('restaurant')     .agg({'restaurant':'count', 'timestamp':'mean'})\
+        .rename(columns={'restaurant':'count','timestamp':'mean'})\
+        .reset_index()
+    dataframe['mean_hour'] = pd.to_datetime(dataframe['mean'], unit='s')
+    dataframe['mean_hour'] = dataframe['mean_hour'].dt.tz_localize('UTC').dt.tz_convert(TIME_ZONE)
+    return dataframe[dataframe['count'] >= min_threshold]
+
+def print_dataframe(dataframe, column):
+    dataframe['mean_hour'] = dataframe['mean_hour'].dt.strftime('%H:%M')
+    result = ''
+    for _, row in dataframe.iterrows():
+        result += row['restaurant'] + '\t' + str(row[column]) + '\n'
+    return result
+
+def get_n_most_popular_restaurants(dataframe, n=10):
+    restaurants = dataframe.sort_values('count', ascending=False).head(n)
+    restaurants['mean_hour'] = restaurants['mean_hour'].dt.strftime('%H:%M')
+    result = ''
+    for _, row in restaurants.iterrows():
+        result += row['restaurant'] + '\n'
+    return result
+
+def get_n_earliest_restaurants(dataframe, n=10):
+    restaurants = dataframe.sort_values('mean_hour', ascending=True).head(n)
+    return print_dataframe(dataframe=restaurants, column='mean_hour')
+
+def get_n_latest_restaurants(dataframe, n=10):
+    restaurants = dataframe.sort_values('mean_hour', ascending=False).head(n)
+    return print_dataframe(dataframe=restaurants, column='mean_hour')
+
+def filter_restaurants_between_hours(dataframe, start_hour, end_hour):
+    import pytz
+    local_tz = pytz.timezone(TIME_ZONE)
+    start = local_tz.localize(dt.strptime('1983-12-09 ' + start_hour, "%Y-%m-%d %H:%M"))
+    end = local_tz.localize(dt.strptime('1983-12-09 ' + end_hour, "%Y-%m-%d %H:%M"))
+    return dataframe[(dataframe['mean_hour'] >= start) & (dataframe['mean_hour'] < end)]
+
+def get_restaurants_by_average_time(dataframe, start_hour='09:00', end_hour='17:00', n=20):
+    restaurants = filter_restaurants_between_hours(dataframe=dataframe, start_hour=start_hour, end_hour=end_hour)
+    restaurants = restaurants.sort_values('mean_hour', ascending=True).head(n)
+    return print_dataframe(dataframe=restaurants, column='mean_hour')
+
+def get_average_time_for_restaurant(dataframe, restaurant):
+    result = dataframe.loc[dataframe['restaurant'] == restaurant]['mean_hour']
+    if result.size == 0:
+        return ''
+    return str(result.dt.strftime('%H:%M').values[0])
