cee3be1cae508522559fec6d88a94576c81744c9
amir.k <amir.k@taboola.com>
2017-11-14 17:40:11 +0200

    add filter by one month back

--- app.py
+++ app.py
@@ -99,22 +99,23 @@ def slack_event():
 
 @app.route('/slack_options', methods=['POST'])
 def slack_options():
-    json_data = json.dumps({})
-    json_data["options"] = [
-      {
-          "text": "Unexpected sentience",
-          "value": "AI-2323"
-      },
-      {
-          "text": "Bot biased toward other bots",
-          "value": "SUPPORT-42"
-      },
-      {
-          "text": "Bot broke my toaster",
-          "value": "IOT-75"
-      }
-    ]
-    return json.dumps(json_data)
+    json = {
+      "options": [
+        {
+            "text": "Unexpected sentience",
+            "value": "AI-2323"
+        },
+        {
+            "text": "Bot biased toward other bots",
+            "value": "SUPPORT-42"
+        },
+        {
+            "text": "Bot broke my toaster",
+            "value": "IOT-75"
+        }
+      ]
+    }
+    return json.dumps(json)
 
 @app.route('/slack_action', methods=['POST'])
 def slack_action():
