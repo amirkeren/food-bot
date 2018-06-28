1cfdc1da42ff10f7d8b61feb8c222c8254592703
amir.k <amir.k@taboola.com>
2018-06-28 12:34:18 +0300

    change timezone

--- logic.py
+++ logic.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 import pandas as pd
 import time as tm
 
-TIME_ZONE = 'Europe/Helsinki'
+TIME_ZONE = 'Europe/Podgorica'
 
 def accept_message(message):
     to_filter = ['has joined the channel', 'uploaded a file', '?', '#', '@', '(', ')']
